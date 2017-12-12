e82df26acea0a72400d60577c07adf6b845ff708
Tom Smits <tmsmits@gmail.com>
2017-12-12 17:02:33 +0100

    Quick edit

--- src/DatabaseHandler.java
+++ src/DatabaseHandler.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class DatabaseHandler {
+
+    private String databaseName;
+    private String connectionURL;
+    private boolean integratedSecurity;
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+
+    public DatabaseHandler(String databaseName, boolean integratedSecurity) {
+        this.databaseName = databaseName;
+        this.integratedSecurity = integratedSecurity;
+
+        this.connectionURL = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=" + this.databaseName + ";integratedSecurity="+ this.integratedSecurity +";";
+    }
+
+    public void connect()  {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(this.connectionURL);
+
+            System.out.println("Successfully connected!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testConnection()
+    {
+        connect();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
+    public DatabaseHandler databaseHandler;
+
     public static void main(String[] args) {
-	// write your code here
+
+        DatabaseHandler databaseHandler = new DatabaseHandler("Bibliotheek", true);
+
+        databaseHandler.testConnection();
     }
 }
