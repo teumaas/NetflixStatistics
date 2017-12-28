5460611160ece3ac42ad1543568c36ca1fb8b8c5
Sjoerdoh <sjoerdschepers@hotmail.nl>
2017-12-28 20:08:18 +0100

    Added comments regarding to-do's and removed the eventlistener

--- src/UserInterface/Edit/EditAccounts.java
+++ src/UserInterface/Edit/EditAccounts.java
@@ -15,59 +15,65 @@ public class EditAccounts {
         GridBagConstraints constraints = new GridBagConstraints();
         this.content.setLayout(layout);
 
-        JLabel currentName = new JLabel("Huidige naam: ");
+        String[] selectAccountList = {"-Selecteer account-"};
+        JComboBox selectAccount = new JComboBox(selectAccountList);
         constraints.gridx = 0;
         constraints.gridy = 0;
+        this.content.add(selectAccount, constraints);
+
+        JLabel currentName = new JLabel("Huidige naam: ");
+        constraints.gridx = 0;
+        constraints.gridy = 1;
         this.content.add(currentName, constraints);
 
         JTextArea nameValue = new JTextArea("--naam--");
         constraints.gridx = 1;
-        constraints.gridy = 0;
+        constraints.gridy = 1;
         this.content.add(nameValue, constraints);
 
         JLabel currentAdress = new JLabel("Huidig adres: ");
         constraints.gridx = 0;
-        constraints.gridy = 1;
+        constraints.gridy = 2;
         this.content.add(currentAdress, constraints);
 
         JTextArea adressValue = new JTextArea("--adres--");
         constraints.gridx = 1;
-        constraints.gridy = 1;
+        constraints.gridy = 2;
         this.content.add(adressValue, constraints);
 
         JLabel currentHouseNumber = new JLabel("Huidig huisnummer: ");
         constraints.gridx = 0;
-        constraints.gridy = 2;
+        constraints.gridy = 3;
         this.content.add(currentHouseNumber, constraints);
 
         JTextArea houseNumberValue = new JTextArea("--huisnummer--");
         constraints.gridx = 1;
-        constraints.gridy = 2;
+        constraints.gridy = 3;
         this.content.add(houseNumberValue, constraints);
 
         JLabel currentPostalCode = new JLabel("Huidige postcode: ");
         constraints.gridx = 0;
-        constraints.gridy = 3;
+        constraints.gridy = 4;
         this.content.add(currentPostalCode , constraints);
 
         JTextArea postalCodeValue = new JTextArea("--postcode--");
         constraints.gridx = 1;
-        constraints.gridy = 3;
+        constraints.gridy = 4;
         this.content.add(postalCodeValue, constraints);
 
         JLabel currentPlace = new JLabel("Huidige woonplaats: ");
         constraints.gridx = 0;
-        constraints.gridy = 4;
+        constraints.gridy = 5;
         this.content.add(currentPlace, constraints);
 
         JTextArea placeValue = new JTextArea("--woonplaats--");
         constraints.gridx = 1;
-        constraints.gridy = 4;
+        constraints.gridy = 5;
         this.content.add(placeValue, constraints);
 
         JButton submit = new JButton("Opslaan");
         constraints.gridx = 0;
-        constraints.gridy = 5;
+        constraints.gridy = 6;
         this.content.add(submit, constraints);
 
     }
